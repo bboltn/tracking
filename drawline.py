fd2cef678803a2b027e6b6456f080a4a9d5c84e5
Brian Bolton <brian.bolton@gmail.com>
2014-03-07 19:44:27 -0500

    WIP

---
+++ drawline.py
@@ -0,0 +1,28 @@
+#!./bin/python
+
+import numpy as np
+import cv2
+import time
+
+# Create a black image
+img = np.zeros((512,512,3), np.uint8)
+
+# Draw a diagonal blue line with thickness of 5 px
+cv2.line(img,(0,0),(511,511),(255,0,0),5)
+
+cv2.rectangle(img,(384,0),(510,128),(0,255,0),3)
+
+cv2.circle(img,(447,63), 63, (0,0,255), -1)
+
+cv2.ellipse(img,(256,256),(100,50),0,0,180,255,-1)
+
+cv2.ellipse(img,(256,256),(100,50),0,0,180,255,-1)
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img,'Hi',(10,500), font, 1,(255,255,255),2)
+
+cv2.imshow('image',img)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
